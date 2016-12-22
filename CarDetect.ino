642af9c005caa47d97eccaaf0fd0b3050910d69f
shimniok@gmail.com <shimniok@gmail.com>
2016-12-22 09:35:04 -0700

    moved event lap out to cardetect... switching back to single lap event

--- CarDetect.ino
+++ CarDetect.ino
@@ -8,11 +8,11 @@ void detectInit() {
 
 void handleCar0() {
   timerLap(0);
-  eventSet(EVENT_LAP_0);
+  eventSet(EVENT_LAP);
 }
 
 void handleCar1() {
   timerLap(1);
-  eventSet(EVENT_LAP_1);
+  eventSet(EVENT_LAP);
 }
 
