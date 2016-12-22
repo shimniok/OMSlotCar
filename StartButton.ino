f9c5cf2705d84a30befab8ed805d656733c94984
shimniok@gmail.com <shimniok@gmail.com>
2016-12-22 12:56:06 -0700

    fixed waitforstartbutton logic

--- StartButton.ino
+++ StartButton.ino
@@ -1,17 +1,22 @@
 
 void startInit() {
+  pinMode(BUTTON, INPUT_PULLUP);
   enableInterrupt(BUTTON, handleStart, FALLING);
-}
-
-void handleStart() {
-  eventSet(EVENT_BUTTON);
+  pinMode(13, OUTPUT);
 }
 
 void waitForStartButton() {
-  while (getEvent(EVENT_BUTTON));
+  while (getEvent(EVENT_BUTTON) == 0) {
+    delay(200);
+    digitalWrite(13, digitalRead(13) ? LOW : HIGH);
+  }
 }
 
 boolean startPressed() {
   return (getEvent(EVENT_BUTTON)); 
+}
+
+void handleStart() {
+  eventSet(EVENT_BUTTON);
 }
 
